cdaed20967033c337471af4fa45e87a505097fe7
Kyle Maxwell <kyle@kylemaxwell.com>
2010-02-21 14:19:25 -0800

    Version bump to 0.0.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,10 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require "rubygems"
 require 'tablegrok'
 require 'spec'
 require 'spec/autorun'
+require "pp"
 
 Spec::Runner.configure do |config|
   
--- spec/tablegrok_spec.rb
+++ spec/tablegrok_spec.rb
@@ -1,7 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Tablegrok" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  it "should return a good parse of the table" do
+    expected = eval(File.read(File.dirname(__FILE__) + "/expected.rb"))
+    html = File.open(File.dirname(__FILE__) + "/sample.html")
+    Tablegrok.new.parse(html).should == expected
   end
 end
